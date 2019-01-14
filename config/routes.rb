272a7bbf0f503e6886e71e6b725b8b7d2441050b
Luis Hankel <kaes3kuch3n@gmail.com>
2019-01-14 10:59:49 +0100

    Generated scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :subs
+  resources :orders
+  resources :sauces
+  resources :main_ingredients
+  resources :ingredients
+  resources :breads
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
