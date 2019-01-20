b5c12e47fc5604c211f0cf35c7cc5253bd5b8475
Luis Hankel <kaes3kuch3n@gmail.com>
2019-01-21 00:39:22 +0100

    Added database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bread.create([
+    {name: "Honey Oat", count: 52},
+    {name: "Flatbread", count: 23},
+    {name: "Italian Cheese & Oregano", count: 14},
+    {name: "Whole Wheat", count: 34}
+])
+
+MainIngredient.create([
+    {name: "Chicken Teriaki", count: 32},
+    {name: "Beef", count: 13},
+    {name: "Ham & Bacon", count: 42},
+    {name: "Tuna", count: 37}
+])
+
+Ingredient.create([
+    {name: "Lettuce", count: 52},
+    {name: "Corn", count: 46},
+    {name: "Cucumber", count: 38},
+    {name: "Pepperoni", count: 32},
+    {name: "Bell Pepper", count: 39},
+    {name: "Tomatoes", count: 27},
+])
+
+Sauce.create([
+    {name: "Honey Mustard", count: 35},
+    {name: "BBQ", count: 29},
+    {name: "Sweet Onion", count: 17},
+    {name: "Mayonnaise", count: 25}
+])
