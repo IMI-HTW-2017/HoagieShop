d79ecf3b0265016674315c9943206a40be948f6b
Luis Hankel <kaes3kuch3n@gmail.com>
2019-02-02 15:18:34 +0100

    Some refactoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 #https://blog.teamtreehouse.com/static-pages-ruby-rails
 class PagesController < ApplicationController
   def show
+    @order = current_order
     render template: "pages/#{params[:page]}"
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
 
-  Billing_Methods = { cash: 'Cash', visa: 'VISA', paypal: 'PayPal'}
+  BILLING_METHODS = {cash: 'Cash', visa: 'VISA', paypal: 'PayPal'}
 
   def subs_list
     Sub.where(order_id: id)
